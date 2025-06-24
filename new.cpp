385fcc17d4652c8bec4b99bd04554c2bdae6dfc7
Tawhid Hasan <tawhidhasan88@gmail.com>
2025-06-24 13:50:06 +0600

    Added new.cpp for a calculator

---
+++ new.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+int main() {
+    double num1, num2;
+    char op;
+
+    std::cout << "Enter first number: ";
+    std::cin >> num1;
+
+    std::cout << "Enter operator (+, -, *, /): ";
+    std::cin >> op;
+
+    std::cout << "Enter second number: ";
+    std::cin >> num2;
+
+    double result;
+    switch(op) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            if(num2 != 0)
+                result = num1 / num2;
+            else {
+                std::cout << "Error: Division by zero!" << std::endl;
+                return 1;
+            }
+            break;
+        default:
+            std::cout << "Invalid operator!" << std::endl;
+            return 1;
+    }
+
+    std::cout << "Result: " << result << std::endl;
+    return 0;
+}
